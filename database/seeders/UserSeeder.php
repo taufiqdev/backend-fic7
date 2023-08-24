6c59bdb73d61f3da4d9992a132800af3a681ed7c
gmb sumber <git.gmb@gmail.com>
2023-08-24 08:53:06 +0700

    tambah

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //\App\Models\User::factory(10)->create();
+
+        User::factory(10)->create();
+        User::create([
+            'name'=>'Taufiq B',
+            'email'=>'taufiqb@gmail.com',
+            'email_verified_at'=>now(),
+            'password'=>'12345',
+        ]);
+
+        //
+    }
+}
